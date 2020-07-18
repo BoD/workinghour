aaeebf3d534c3ba5b3e1ef55956416db971a910e
BoD <BoD@JRAF.org>
2020-07-18 14:17:12 +0200

    Fix timezone bug on MacOS

--- src/commonMain/kotlin/org/jraf/workinghour/activitydetection/ActivityDetector.kt
+++ src/commonMain/kotlin/org/jraf/workinghour/activitydetection/ActivityDetector.kt
@@ -66,7 +66,7 @@ class ActivityDetector(
                 isActive = newMouseLocation != latestMouseLocation
                 latestMouseLocation = newMouseLocation
 
-                delay(monitoringPeriod.toLongMilliseconds())
+                delay(monitoringPeriod)
             }
         }
     }
